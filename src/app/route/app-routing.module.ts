ad44dc3a287b84c6c024abe3fada45677555c8ac
臧俞淞 <zangyusong@mininglamp.com>
2019-02-17 17:26:15 +0800

    路由预加载

--- src/app/route/app-routing.module.ts
+++ src/app/route/app-routing.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { ErrorComponent } from '../component/4o4/error.component'
+import { AppPreloadingStrategyService } from '../service/app-preloading-strategy.service'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'login', loadChildren: './login-routing.module#LoginRoutingModule' },
-  { path: 'home', loadChildren: './home-routing.module#HomeRoutingModule' },
-  { path: 'list', loadChildren: './list-routing.module#ListRoutingModule' },
-  { path: 'detail/:id', loadChildren: './detail-routing.module#DetailRoutingModule' },
-  { path: 'parent', loadChildren: './parent-routing.module#ParentRoutingModule' },
+  { path: 'login', loadChildren: './login-routing.module#LoginRoutingModule', data: { preload: true } },
+  { path: 'home', loadChildren: './home-routing.module#HomeRoutingModule', data: { preload: true } },
+  { path: 'list', loadChildren: './list-routing.module#ListRoutingModule', data: { preload: true } },
+  { path: 'detail/:id', loadChildren: './detail-routing.module#DetailRoutingModule', data: { preload: true } },
+  { path: 'parent', loadChildren: './parent-routing.module#ParentRoutingModule', data: { preload: true } },
   { path: '**', redirectTo: '/404', pathMatch: 'full' },
   { path: '404', component: ErrorComponent }
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: AppPreloadingStrategyService })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
