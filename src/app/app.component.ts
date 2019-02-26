c60fe21fbfe54f6da8ba8655fe46d212bcb81809
臧俞淞 <zangyusong@mininglamp.com>
2019-02-26 11:58:55 +0800

    添加路由监听

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { Router, NavigationEnd, NavigationStart } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        console.log('NavigationStart')
+      } else if (event instanceof NavigationEnd) {
+        console.log('NavigationEnd')
+      }
+    })
+  }
+}
